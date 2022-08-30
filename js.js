e1eb2d46d3daba69082e9b313fd4a835b5114f7d
Helbob <millehelbo@outlook.dk>
2022-08-30 12:36:30 +0200

    i got it to prints out yay

--- js.js
+++ js.js
@@ -4,17 +4,38 @@ document.querySelector("#generate").addEventListener("click", generate);
 
 function generate() {
   let options = document.querySelector("#options");
-
+  let optionsValue = document.querySelector("#options").value;
   const input = document.querySelector("#input");
-  let inputValue = input.value;
+  let inputValue = document.querySelector("#input").value;
   let output = document.querySelector("#output");
   let outputValue = output.value;
 
-  let input2;
+  let input2 = "";
 
-  if (options == "firstname") {
-    output.value =
-      input2.charAt(0).toUpperCase() + input2.substring(1).toLowerCase();
-    console.log("hej");
+  if (optionsValue == "firstname") {
+    let result =
+      inputValue.charAt(0).toUpperCase() +
+      inputValue.substring(1).toLowerCase();
+    output.textContent = result;
+  } else if (optionsValue == "filename") {
+    let result = inputValue.includes(".jpg") || inputValue.includes(".png");
+    output.textContent = result;
+  } else if (optionsValue == "fullname") {
+    let result = inputValue.split(" ")[1];
+    output.textContent = result;
+  } else if (optionsValue == "fullnamelenght") {
+    let result = inputValue.substring(0, inputValue.indexOf(" ")).length;
+    output.textContent = result;
+  } else if (optionsValue == "fullnamemiddle") {
+    let result = inputValue.substring(
+      inputValue.indexOf(" ") + 1,
+      inputValue.lastIndexOf(" ")
+    );
+    output.textContent = result;
+  } else if (optionsValue == "passw") {
+    //let result = inputValue.
+  } else if (optionsValue == "thirdupper") {
+    // let result = inputValue.
+  } else if (optionsValue == "hyphen") {
   }
 }
